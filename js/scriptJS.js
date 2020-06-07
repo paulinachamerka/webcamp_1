71241c0cf0b621371db3cba5943bfc6390efc827
paulinachamerka <56074180+paulinachamerka@users.noreply.github.com>
2020-06-07 19:50:25 +0200

    Add files via upload

--- js/scriptJS.js
+++ js/scriptJS.js
@@ -1,19 +1,55 @@
-function buttonfunction()
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+btn.onclick = function()
 {
-		if(document.getElementById("wybrozmiar").selectedIndex=="0")
+	if(document.getElementById("wybrozmiar").selectedIndex=="0")
 	{
-	 alert("wybierz rozmiar");
+		modal.style.display = "block";
 	}
-	 else alert("dodano do koszyka");
+	else modal2.style.display = "block";
 }
 
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar-menu");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+span.onclick = function()
+{
+	if(document.getElementById("wybrozmiar").selectedIndex=="0")
+	{
+			modal.style.display = "none";
+	}
 }
+
+span2.onclick = function()
+{
+	if(document.getElementById("wybrozmiar").selectedIndex=="1" || document.getElementById("wybrozmiar").selectedIndex=="2" || document.getElementById("wybrozmiar").selectedIndex=="3")
+	{
+			modal2.style.display = "none";
+	}
+}
+window.onclick = function(event) 
+{
+	if(document.getElementById("wybrozmiar").selectedIndex=="0")
+	{
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+	else 
+	{
+		if (event.target == modal2) {
+			modal2.style.display = "none";
+		}
+	}
+}
+
+const hamburger = documen.querySelector('.hamburger');
+const nav = documen.guerySelector('.navbar-menu');
+
+const handleClick = ()
+{
+	hamburger.classlist.toggle('hamburger::active');
+}
+
+hamburger.addEventListener('click', handleClick);
